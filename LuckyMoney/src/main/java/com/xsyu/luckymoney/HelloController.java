759b7515a305064c0d42ad48a725a2325cd7db6b
HYDYD_WIN <sdwang3@qq.com>
2019-06-18 21:01:12 +0800

    从删库到跑路

--- LuckyMoney/src/main/java/com/xsyu/luckymoney/HelloController.java
+++ LuckyMoney/src/main/java/com/xsyu/luckymoney/HelloController.java
@@ -2,8 +2,8 @@ package com.xsyu.luckymoney;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
@@ -12,12 +12,15 @@ import java.math.BigDecimal;
  * 2019/6/18 12:48
  */
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
     @Autowired
     private LimitConfig limitConfig;
 
-    @GetMapping("/hello")
-    public String say(){
-        return "Hello!" + limitConfig.getMinMoney() + " " + limitConfig.getDescription();
+    @GetMapping("/say")
+    public String say(@RequestParam(value = "id", required = false, defaultValue = "0") Integer id){
+        //return "index";
+        return "id:" + id;
+        //return "Hello!" + limitConfig.getMinMoney() + " " + limitConfig.getDescription();
     }
 }
