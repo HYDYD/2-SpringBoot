11ae07d9f48cd4eca8627bc9192055e9fee0a4bf
HYDYD_WIN <sdwang3@qq.com>
2019-06-20 18:57:46 +0800

    问题解决 add Postman 7-1

---
+++ LuckyMoney/src/main/java/com/xsyu/luckymoney/LuckymoneyController.java
@@ -0,0 +1,76 @@
+package com.xsyu.luckymoney;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by HYDYD.
+ * 2019/6/20 16:56
+ */
+@RestController
+@RequestMapping("/luckymoneys")
+public class LuckymoneyController {
+    @Autowired
+    private LuckymoneyDao luckymoneyDao;
+
+    /**
+     * 获取红包列表
+     * @return
+     */
+    @GetMapping("/getlist")
+    public List<Luckymoney> getList(){
+        return luckymoneyDao.findAll();
+    }
+
+    /**
+     * 创建红包（发红包）
+     * @param producer
+     * @param money
+     * @return
+     */
+    @PostMapping("/createLM")
+    public Luckymoney createLM(@RequestParam("producer") String producer,
+                               @RequestParam("money")BigDecimal money) {
+        Luckymoney luckymoney = new Luckymoney();
+        luckymoney.setMoney(money);
+        luckymoney.setProducer(producer);
+        return luckymoneyDao.save(luckymoney);
+    }
+
+    /**
+     * 通过ID查询红包
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public Luckymoney findById(@PathVariable("id") Integer id){
+        return luckymoneyDao.findById(id).orElse(null);
+    }
+
+
+    /**
+     * 更新红包（领红包）
+     *
+     * PUT请求：如果两个请求相同，后一个请求会把第一个请求覆盖掉。（所以PUT用来改资源）
+     * Post请求：后一个请求不会把第一个请求覆盖掉。（所以Post用来增资源）
+     * @param id
+     * @param consumer
+     * @return
+     */
+    @PutMapping("/{id}")
+    public Luckymoney update(@PathVariable("id") Integer id,
+                             @RequestParam("consumer") String consumer){
+        Optional<Luckymoney> optional = luckymoneyDao.findById(id);
+        if (optional.isPresent()) {
+            Luckymoney luckymoney = optional.get();
+            luckymoney.setConsumer(consumer);
+            return luckymoneyDao.save(luckymoney);
+        }
+        return null;
+    }
+
+}
---
+++ LuckyMoney/src/main/java/com/xsyu/luckymoney/LuckymoneyDao.java
@@ -0,0 +1,10 @@
+package com.xsyu.luckymoney;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by HYDYD.
+ * 2019/6/20 16:57
+ */
+public interface LuckymoneyDao extends JpaRepository<Luckymoney, Integer> {
+}
